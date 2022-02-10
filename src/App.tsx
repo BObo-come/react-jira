3ee3ecc7b94b1d17b22687387f55dc2e515c5888
zhaoxin <123456>
2022-02-10 18:25:09 +0800

    添加登录模拟

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 // import { ProjectListScreen } from './screens/project-list';
-import {TsReactTest} from "./screens/user-array/try-user-array"
+// import {TsReactTest} from "./screens/user-array/try-user-array"
+import {LoginScreen} from './screens/login'
 
 function App() {
   return (
     <div className="App">
         {/* <ProjectListScreen/> */}
-        <TsReactTest/>
+        {/* <TsReactTest/> */}
+        <LoginScreen/>
     </div>
   );
 }
