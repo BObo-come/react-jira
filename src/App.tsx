446a80a2f9486c5d8816bc169f023d58bd1d509c
zhaoxin <123456>
2022-02-13 15:11:25 +0800

    添加登录注册系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
+import { AuthenticatedApp } from './authenticated-app';
+import { useAuth } from './context/auth-context';
+import { UnauthenticatedApp } from './unauthenticated-app';
 // import { ProjectListScreen } from './screens/project-list';
 // import {TsReactTest} from "./screens/user-array/try-user-array"
-import {LoginScreen} from './screens/login'
+// import {LoginScreen} from './screens/login'
 
 function App() {
+  const {user} = useAuth()
   return (
     <div className="App">
-        {/* <ProjectListScreen/> */}
-        {/* <TsReactTest/> */}
-        <LoginScreen/>
+        {user? <AuthenticatedApp/>:<UnauthenticatedApp/>}
     </div>
   );
 }
